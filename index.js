5de03ecb87ef0db4818e32ead3560bc65d2ea2d5
Tyler Lee <125tlee@frhsd.com>
2024-10-17 11:47:38 -0400

    Edit #2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const numberColor = document.getElementById("")
 function makeGray()
 {
-
+    numberColor.style.color = "gray";
 }
 function makeGreen()
 {
-
+    numberColor.style.color = "green";
 }
 function makePurple()
 {
-
+    numberColor.style.color = "purple";
+}
+const numberValue = 0;
+function minusOne()
+{
+    document.getElementById("currNumber").innerHTML = numberValue-1;
+}
+function plusOne()
+{
+    document.getElementById("currNumber").innerHTML = numberValue+1;
 }
